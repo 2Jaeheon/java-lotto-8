41b09e85fbc33717ef0228da9c3e4a27ead9f3b3
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-11-01 14:20:32 +0900

    feat(domain): 구입 금액에 해당하는 로또 개수를 계산
    구입 금액에 해당하는 로또 개수를 계산하는 로직을 구현 및 검증
    
    세부 사항:
    - 로또 가격보다 작으면 예외 발생
    - 로또 가격(1000)으로 나눴을 때 나누어 떨어지지 않으면 예외 발생
    - 로또 개수를 반환하는 lottoCount() 메서드 구현
    - Money 테스트
    - Money 클래스

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,4 +1,32 @@
 package lotto.domain;
 
 public class Money {
+    private static final int PRICE_PER_LOTTO = 1000;
+    private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String ERROR_BELOW_MIN = ERROR_PREFIX + "로또 구입 금액이 1000원 미만입니다";
+    private static final String ERROR_NOT_MULTIPLE = ERROR_PREFIX + "로또 구입 금액이 1000원 단위가 아닙니다";
+
+    private final int amount;
+
+    public Money(int amount) {
+        validateAmountNotBelowLottoPrice(amount);
+        validateAmountIsMultipleOfLottoPrice(amount);
+        this.amount = amount;
+    }
+
+    public int lottoCount() {
+        return amount / PRICE_PER_LOTTO;
+    }
+
+    private void validateAmountIsMultipleOfLottoPrice(int amount) {
+        if (amount % PRICE_PER_LOTTO != 0) {
+            throw new IllegalArgumentException(ERROR_NOT_MULTIPLE);
+        }
+    }
+
+    private void validateAmountNotBelowLottoPrice(int amount) {
+        if (amount < PRICE_PER_LOTTO) {
+            throw new IllegalArgumentException(ERROR_BELOW_MIN);
+        }
+    }
 }
---
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+    private static final int VALID_AMOUNT = 8000;
+
+    @DisplayName("1000원 단위가 아니면 예외를 발생한다")
+    @Test
+    void shouldThrowWhenNotMultipleOfThousand() {
+        assertThatThrownBy(() -> new Money(1250))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 구입 금액이 1000원 단위가 아닙니다");
+    }
+
+    @DisplayName("1000원 미만이면 예외를 발생한다")
+    @Test
+    void shouldThrowWhenIsLessThan1000() {
+        assertThatThrownBy(() -> new Money(900))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 구입 금액이 1000원 미만입니다");
+    }
+
+    @DisplayName("금액에서 살 수 있는 로또의 장수를 계산한다")
+    @Test
+    void shouldReturnLottoTicketCount() {
+        //given
+        Money money = new Money(VALID_AMOUNT);
+
+        //when
+        int ticketCount = money.lottoCount();
+
+        //then
+        assertThat(ticketCount).isEqualTo(8);
+    }
+}
