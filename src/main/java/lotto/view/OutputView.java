8e0908064d560a743de98a27abfb706dc2efd6d4
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-11-01 16:55:45 +0900

    feat(view): 구입한 로또 개수 출력
    구입한 로또의 개수를 출력하는 로직 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,9 @@
 package lotto.view;
 
 public class OutputView {
+    private static final String PURCHASE_MESSAGE_FORMAT = "%d개를 구매했습니다.";
+
+    public static void printPurchaseCount(int count) {
+        System.out.printf(PURCHASE_MESSAGE_FORMAT + "\n", count);
+    }
 }
